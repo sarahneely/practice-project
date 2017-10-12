1ef53967255c3f76c60383f90f05fe29944067bc
sarahneely <sporter1990@hotmail.com>
2017-10-12 15:32:26 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+
+constructor(
+  private http: HttpClient,
+) {}
+
+ngOnInit() {
+  this.http.get<Post>('https://www.reddit.com/r/php/search.json?q=cats&limit=5');
+    .subscribe(postList=> {
+      this.postList = postList.data.children;
+      console.log('this.postList', this.postList);
+    });
+  }
+}
+
+
+export class Post {
+  kind: string;
+  data: PostData;
+}
+
+export class PostData {
+  children: PostChildren[];
+}
+
+export class PostChildren {
+  data: PostChildrenData;
+}
+
+export class PostChildrenData {
+
+}
